f28f4cd560d2102002dc859421f0f3fcb90f7f30
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-05-28 20:20:00 +0530

    added hedder bar and status bar styles

--- StickerSmash/app/(tabs)/_layout.tsx
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -4,6 +4,14 @@ import {Ionicons} from "@expo/vector-icons";
 export default function TabsLayout() {
   return <Tabs screenOptions={{
     tabBarActiveTintColor: "#ffd33d",
+    headerStyle: {
+      backgroundColor: "#25292e",
+    },
+    headerShadowVisible: false,
+    headerTintColor: "#fff",
+    tabBarStyle: {
+      backgroundColor: "#25292e",
+    },
   }}>
   <Tabs.Screen
     
