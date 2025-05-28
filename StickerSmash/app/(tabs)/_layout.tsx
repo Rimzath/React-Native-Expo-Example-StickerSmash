76fd9e0ef0824a33a919fef64187593f71d14d6d
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-05-28 20:09:05 +0530

    added tabs icon and styles

--- StickerSmash/app/(tabs)/_layout.tsx
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -1,12 +1,34 @@
 import { Tabs } from "expo-router";
+import {Ionicons} from "@expo/vector-icons";
 
 export default function TabsLayout() {
-  return <Tabs>
+  return <Tabs screenOptions={{
+    tabBarActiveTintColor: "#ffd33d",
+  }}>
   <Tabs.Screen
+    
     name="index"
     options={{
       headerTitle: "Sticker Smash", 
-      headerTitleAlign: "center"
+      tabBarIcon:({focused,color}) => <Ionicons 
+    name={focused? "home-sharp" : "home-outline"}
+    color={color} 
+    size={30}/>,
+    headerTitleAlign: "center"
+      
+    }}
+  />
+  <Tabs.Screen
+    
+    name="about"
+    options={{
+      headerTitle: "Sticker Smash", 
+      tabBarIcon:({focused,color}) => <Ionicons 
+    name={focused? "information-circle" : "information-circle-outline"}
+    color={color} 
+    size={30}/>,
+    headerTitleAlign: "center"
+      
     }}
   />
 </Tabs>
