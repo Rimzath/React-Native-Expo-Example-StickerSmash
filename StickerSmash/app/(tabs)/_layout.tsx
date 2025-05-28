a872581884e825c30a2a8bd0db74c047d69ba8ea
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-05-28 13:35:47 +0530

    added new tab and move the file into it snd remove double header

---
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -0,0 +1,13 @@
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return <Tabs>
+  <Tabs.Screen
+    name="index"
+    options={{
+      headerTitle: "Sticker Smash",
+      headerLeft: () => <></>, 
+    }}
+  />
+</Tabs>
+}
