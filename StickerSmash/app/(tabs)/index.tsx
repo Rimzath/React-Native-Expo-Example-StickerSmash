fc055c0893a46d46c042bc88744c22f78454697a
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-05-29 19:58:38 +0530

    Created Save and reset buttons and added to index page

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { View } from "react-native";
-import { StyleSheet } from "react-native";
-import ImageViewer from "@/components/ImageViewer";
-import Button from "@/components/Button";
+import { StyleSheet , View} from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+import IconButton from "@/components/IconButton";
+import CircleButton from "@/components/CircleButton";
 
 const PlaceholderImage = require("../../assets/images/background-image.png")
 
@@ -28,13 +29,42 @@ export default function Index() {
       alert("You did not select any image.");
     }
   };
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    //
+  };
+
+  const onModalClose = () => {
+    //
+  };
+
+  const onSaveImageAsync = async () => {
+    //
+    
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={selectedImage ||PlaceholderImage}/>
       </View>
 
-      {showAppOptions ?(<View/>) :(<View style={styles.footerContainer}>
+      {showAppOptions ?(<View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+        ) :(
+        <View style={styles.footerContainer}>
         <Button
         onPress={pickImageAsync} 
         label="Choose photo" 
@@ -60,6 +90,14 @@ const styles = StyleSheet.create({
   footerContainer:{
     flex: 1/3,
     alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
   },
 
 });
